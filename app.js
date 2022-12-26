0d73057387aaad9cdb7adb72e9bb82b132f266e9
quan195 <naquan@cmcglobal.vn>
2022-12-26 10:28:47 +0700

    initial commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const fs = require("fs");
+const express = require("express");
+
+const app = express();
+
+const tours = fs.
+
+app.get("/api/v1/tours", (req, res) => {
+  
+});
+
+const port = 8000;
+
+app.listen(port, () => {
+  console.log(`App running on port ${port}...`);
+});
