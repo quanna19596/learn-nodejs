0bf18bdf6680ef12c14b00264eef4a91e5883417
quan195 <naquan@cmcglobal.vn>
2022-12-26 17:15:45 +0700

    57

--- app.js
+++ app.js
@@ -3,10 +3,81 @@ const express = require("express");
 
 const app = express();
 
-const tours = fs.
+app.use(express.json());
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
 
 app.get("/api/v1/tours", (req, res) => {
-  
+  res.status(200).json({
+    stats: "success",
+    data: {
+      tours,
+    },
+  });
+});
+
+app.get("/api/v1/tours/:id", (req, res) => {
+  const id = +req.params.id;
+  const tour = tours.find((tour) => tour.id === id);
+
+  if (!tour) {
+    return res.status(404).json({ status: "failed", message: "Invalid ID" });
+  }
+
+  res.status(200).json({
+    stats: "success",
+    data: {
+      tour,
+    },
+  });
+});
+
+app.post("/api/v1/tours", (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = { id: newId, ...req.body };
+
+  tours.push(newTour);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: "success",
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
+});
+
+app.patch("/api/v1/tours/:id", (req, res) => {
+  const id = +req.params.id;
+
+  if (id > tours.length) {
+    return res.status(404).json({ status: "failed", message: "Invalid ID" });
+  }
+  res.status(200).json({
+    status: "success",
+    data: {
+      tour: "<Updated tour here...>",
+    },
+  });
+});
+
+app.delete("/api/v1/tours/:id", (req, res) => {
+  const id = +req.params.id;
+
+  if (id > tours.length) {
+    return res.status(404).json({ status: "failed", message: "Invalid ID" });
+  }
+
+  res.status(204).json({
+    status: "success",
+    data: null,
+  });
 });
 
 const port = 8000;
