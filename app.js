d9023a15a665f8f5624d207a0a6036ac0e92c662
quan195 <naquan@cmcglobal.vn>
2022-12-27 17:45:31 +0700

    Learning 67

--- app.js
+++ app.js
@@ -1,87 +1,26 @@
-const fs = require("fs");
 const express = require("express");
+const morgan = require("morgan");
+
+const tourRouter = require("./routes/tourRoutes");
+const userRouter = require("./routes/userRoutes");
 
 const app = express();
 
+app.use(morgan("dev"));
 app.use(express.json());
+app.use(express.static(`${__dirname}/public`))
 
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
-);
-
-app.get("/api/v1/tours", (req, res) => {
-  res.status(200).json({
-    stats: "success",
-    data: {
-      tours,
-    },
-  });
+app.use((req, res, next) => {
+  console.log("Hello middleware");
+  next();
 });
 
-app.get("/api/v1/tours/:id", (req, res) => {
-  const id = +req.params.id;
-  const tour = tours.find((tour) => tour.id === id);
-
-  if (!tour) {
-    return res.status(404).json({ status: "failed", message: "Invalid ID" });
-  }
-
-  res.status(200).json({
-    stats: "success",
-    data: {
-      tour,
-    },
-  });
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
 });
 
-app.post("/api/v1/tours", (req, res) => {
-  const newId = tours[tours.length - 1].id + 1;
-  const newTour = { id: newId, ...req.body };
+app.use("/api/v1/tours", tourRouter);
+app.use("/api/v1/users", userRouter);
 
-  tours.push(newTour);
-  fs.writeFile(
-    `${__dirname}/dev-data/data/tours-simple.json`,
-    JSON.stringify(tours),
-    (err) => {
-      res.status(201).json({
-        status: "success",
-        data: {
-          tour: newTour,
-        },
-      });
-    }
-  );
-});
-
-app.patch("/api/v1/tours/:id", (req, res) => {
-  const id = +req.params.id;
-
-  if (id > tours.length) {
-    return res.status(404).json({ status: "failed", message: "Invalid ID" });
-  }
-  res.status(200).json({
-    status: "success",
-    data: {
-      tour: "<Updated tour here...>",
-    },
-  });
-});
-
-app.delete("/api/v1/tours/:id", (req, res) => {
-  const id = +req.params.id;
-
-  if (id > tours.length) {
-    return res.status(404).json({ status: "failed", message: "Invalid ID" });
-  }
-
-  res.status(204).json({
-    status: "success",
-    data: null,
-  });
-});
-
-const port = 8000;
-
-app.listen(port, () => {
-  console.log(`App running on port ${port}...`);
-});
+module.exports = app;
