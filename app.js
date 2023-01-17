6c91e1a36790c97d21e66a0a87595cc70c051bda
quan195 <naquan@cmcglobal.vn>
2023-01-17 17:04:32 +0700

    Done 113

--- app.js
+++ app.js
@@ -6,6 +6,13 @@ const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
+app.all('*', (req, res, next) => {
+  res.status(404).json({
+    status: 'failed',
+    message: `Can't find ${req.originalUrl} on this server!`,
+  });
+});
+
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
